d826bc07812cd39fc43b24795be0345b9961da20
adi142003 <103492140+adi142003@users.noreply.github.com>
2024-04-21 16:55:01 +0530

    Update TeacherRepository.java

--- capstone_management/src/main/java/com/capstone/capstone_management/repository/TeacherRepository.java
+++ capstone_management/src/main/java/com/capstone/capstone_management/repository/TeacherRepository.java
@@ -5,8 +5,10 @@ import com.capstone.capstone_management.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+public interface TeacherRepository extends JpaRepository<Teacher, String> {
 
     Teacher findByUseraccount(UserAccount trn);
+    Optional<Teacher> findById(Long id);
 }
