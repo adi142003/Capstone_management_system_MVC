cc017845c5b6b212d34947bc9a11a6d392d4b723
adi142003 <103492140+adi142003@users.noreply.github.com>
2024-04-21 16:59:00 +0530

    Update TeacherServiceImpl.java

--- capstone_management/src/main/java/com/capstone/capstone_management/service/impl/TeacherServiceImpl.java
+++ capstone_management/src/main/java/com/capstone/capstone_management/service/impl/TeacherServiceImpl.java
@@ -1,12 +1,17 @@
 package com.capstone.capstone_management.service.impl;
 
 import com.capstone.capstone_management.models.Teacher;
+import com.capstone.capstone_management.models.Project;
 import com.capstone.capstone_management.models.UserAccount;
 import com.capstone.capstone_management.repository.*;
 import com.capstone.capstone_management.service.TeacherService;
+import org.apache.catalina.User;
+import org.hibernate.mapping.List;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.time.Year;
+
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
@@ -16,13 +21,29 @@ public class TeacherServiceImpl implements TeacherService {
     private PanelRepository panelRepository;
     private ReviewRepository reviewRepository;
     private TimetableRepository timetableRepository;
+    private ProjectRepository projectRepository;
 
-    public TeacherServiceImpl(UserAccountRepository userAccountRepository, TeacherRepository teacherRepository, PanelRepository panelRepository, ReviewRepository reviewRepository, TimetableRepository timetableRepository) {
+    public TeacherServiceImpl(UserAccountRepository userAccountRepository, ProjectRepository projectRepository, TeacherRepository teacherRepository, PanelRepository panelRepository, ReviewRepository reviewRepository, TimetableRepository timetableRepository) {
         this.userAccountRepository = userAccountRepository;
         this.teacherRepository = teacherRepository;
         this.panelRepository = panelRepository;
         this.reviewRepository = reviewRepository;
         this.timetableRepository = timetableRepository;
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public java.util.List<Project> getProjectsByTeacherId(int teacherId){
+        return projectRepository.findByTeacherId(teacherId);
+    }
+
+    @Override
+    public java.util.List<Project> getProjectsByYear(int year){
+        return projectRepository.findByProjectyear(Year.of(year));
+    }
+
+    public java.util.List<Project> getAllProjects() {
+        return projectRepository.findAll();
     }
 
     @Override
@@ -30,13 +51,18 @@ public class TeacherServiceImpl implements TeacherService {
         return teacherRepository.save(teacher);
     }
 
+    @Override
+    public String GetTeacherPassword(String TRN) {
+        Optional<Teacher> t = teacherRepository.findById(TRN);
+        UserAccount s = t.get().getUseraccount();
+        return s.getPassword();
+    }
 
     @Override
     public Teacher getTeacher(String trn) {
         UserAccount s = userAccountRepository.findByUsn(trn);
         return teacherRepository.findByUseraccount(s);
     }
-
     @Override
     public Optional<Teacher> getTeacherbyid(Teacher teacher) {
         return teacherRepository.findById(Long.valueOf(teacher.getId()));
