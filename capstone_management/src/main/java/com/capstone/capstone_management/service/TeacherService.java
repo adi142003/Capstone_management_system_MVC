3a7f35eca8a3d7d94ba5993a813bb9ebd19cb7e5
adi142003 <103492140+adi142003@users.noreply.github.com>
2024-04-21 16:57:17 +0530

    Update TeacherService.java

--- capstone_management/src/main/java/com/capstone/capstone_management/service/TeacherService.java
+++ capstone_management/src/main/java/com/capstone/capstone_management/service/TeacherService.java
@@ -1,12 +1,17 @@
 package com.capstone.capstone_management.service;
 
-import com.capstone.capstone_management.models.Teacher;
-
+import java.util.List;
 import java.util.Optional;
+
+import com.capstone.capstone_management.models.Project;
+import com.capstone.capstone_management.models.Teacher;
 
 public interface TeacherService {
     Teacher saveTeacher(Teacher teacher);
+    String GetTeacherPassword(String TRN);
     Teacher getTeacher(String TRN);
-
+    List<Project> getProjectsByTeacherId(int teacherId);
+    List<Project> getProjectsByYear(int year);
+    List<Project> getAllProjects();
     Optional<Teacher> getTeacherbyid(Teacher teacher);
 }
