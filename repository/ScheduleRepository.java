8feacea87beb1e49865207d030ce47e9d9607fda
Revanth Sreeram Ambati <117745230+revanthsreeram@users.noreply.github.com>
2024-04-26 10:15:13 +0530

    Delete repository directory

--- repository/ScheduleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.capstone_management.repository;
-
-import com.capstone.capstone_management.models.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
-}
